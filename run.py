c1bf92d3e3ea23923f98a9ed3fcb23dfb7068442
Anshika Jain <anshika.jain@vitas.com>
2025-05-14 10:54:41 -0400

    Changes for deployment

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 #Start your Flask application.
 
 from app import create_app
+from app.config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=Config.DEBUG, port=5001)
