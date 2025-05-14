ddcdbb2f39084e854b08e31966ca723e12047bb0
Anshika Jain <anshika.jain@vitas.com>
2025-05-14 13:29:16 -0400

    updated the Secret key name in config

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()  # load .env into environment variable
 
 class Config:
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv( "DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.getenv("FLASK_ENV") == "development"
