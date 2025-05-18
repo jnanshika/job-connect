8d53c4e0b72fca737c7864ea7e72cf559d972af0
Anshika Jain <anshika.jain@vitas.com>
2025-05-18 19:40:14 +0530

    Create document using flasgger and deepmerge

--- app/extensions.py
+++ app/extensions.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from authlib.integrations.flask_client import OAuth
 from flask_migrate import Migrate
+from flasgger import Swagger
 
 db = SQLAlchemy()
 jwt = JWTManager()
 oauth = OAuth()
 migrate = Migrate()
+swagger = Swagger()
